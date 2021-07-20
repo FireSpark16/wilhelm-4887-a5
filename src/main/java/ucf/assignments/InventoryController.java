936a251c40aaa491fdecdc90c6e72d5d56206738
FireSpark16 <xanderwilhelm@gmail.com>
2021-07-19 23:27:29 -0400

    Skeleton code for FXML file created in controller.

--- src/main/java/ucf/assignments/InventoryController.java
+++ src/main/java/ucf/assignments/InventoryController.java
@@ -1,4 +1,59 @@
 package ucf.assignments;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
 public class InventoryController {
+
+    @FXML
+    private TableColumn<?, ?> priceColumn;
+
+    @FXML
+    private TableColumn<?, ?> serialNumberColumn;
+
+    @FXML
+    private TableColumn<?, ?> nameColumn;
+
+    @FXML
+    private TableColumn<?, ?> deleteColumn;
+
+    @FXML
+    private TextField messageBox;
+
+    @FXML
+    private TextField itemPriceBox;
+
+    @FXML
+    private TextField itemSerialNumberBox;
+
+    @FXML
+    private TextField itemNameBox;
+
+    @FXML
+    void addItemClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveButtonClicked(ActionEvent event) {
+
+    }
+
 }
+
